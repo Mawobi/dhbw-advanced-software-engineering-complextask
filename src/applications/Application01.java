f5076613b94ed1d1c497b12d7e7402924bf67828
Mawobi <mawobi2506@gmail.com>
2022-04-23 15:23:11 +0200

    feat: add RandomGenerator

--- src/applications/Application01.java
+++ src/applications/Application01.java
@@ -1,12 +1,15 @@
 package applications;
 
 import util.FileSystemLogger;
+import util.MersenneTwisterFast;
 import util.TSPFileReader;
 
 import java.io.IOException;
 
 public class Application01 {
     private final FileSystemLogger logger;
+    // random generator
+    public final MersenneTwisterFast randomGenerator = new MersenneTwisterFast(System.nanoTime());
 
     public Application01() throws IOException {
         TSPFileReader tspFileReader = new TSPFileReader();
@@ -14,6 +17,7 @@ public class Application01 {
 
         logger = new FileSystemLogger(Application01.class.getName());
         logger.info("Application01 initialized");
+
     }
 
     public static void main(String[] args) throws IOException {
