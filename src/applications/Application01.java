8f95bedcc822b10a30a94db6f5b6969cdc655155
Mawobi <mawobi2506@gmail.com>
2022-04-23 13:34:42 +0200

    -

--- src/applications/Application01.java
+++ src/applications/Application01.java
@@ -1,4 +1,12 @@
 package applications;
 
+import filereader.TSPFileReader;
+
+import java.io.FileNotFoundException;
+
 public class Application01 {
+    public static void main(String[] args) throws FileNotFoundException {
+        TSPFileReader tspFileReader = new TSPFileReader();
+        tspFileReader.readTSPData();
+    }
 }
--- src/filereader/TSPFileReader.java
+++ src/filereader/TSPFileReader.java
@@ -1,31 +1,69 @@
 package filereader;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class TSPFileReader {
 
-    public double[][] readTSPData(String filePath) throws FileNotFoundException {
-        // Create scanner
-        Scanner sc = new Scanner(new File(filePath));
-        sc.useLocale(Locale.US);
-        // Read number of cities
-        int n = sc.nextInt();
-        // Initialize distance matrix
-        double[][] dist = new double[n][n];
-        // Fill distance matrix
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                dist[i][j] = sc.nextDouble();
-                dist[j][i] = dist[i][j];
+    public double[][] readTSPData() throws FileNotFoundException, RuntimeException {
+
+        File file = new File("src/resources/a280.tsp");
+
+        Scanner dimensionScanner = new Scanner(file);
+        int dimension = -1;
+        String line = "";
+
+        while (dimensionScanner.hasNextLine()) {
+            line = dimensionScanner.nextLine().trim();
+            if (line.startsWith("DIMENSION:")) {
+                dimension = Integer.parseInt(line.split(" ")[1]);
+                break;
             }
         }
-        // Close scanner
-        sc.close();
+        dimensionScanner.close();
+
+        if (dimension == -1) {
+            throw new RuntimeException("Could not find dimension in file");
+        }
+
+        double[][] dist = new double[dimension][dimension];
+
+        Scanner nodeScanner = new Scanner(file);
+
+        while (nodeScanner.hasNextLine()) {
+            line = nodeScanner.nextLine().trim();
+            String[] splittedLine = line.split(" ");
+            if(splittedLine.length !=3) {
+                continue;
+            }
+
+            double[] node = new double[3];
+            try {
+                node[0] = Double.parseDouble(splittedLine[0]);
+                node[1] = Double.parseDouble(splittedLine[1]);
+                node[2] = Double.parseDouble(splittedLine[2]);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+
+
+        }
+        nodeScanner.close();
+
+//        while ((line = csvReader.readLine()) != null) {
+//            String[] lineArgs = line.split(",");
+//            for (int i = 0; i < n; i++) {
+//                for (int j = 0; j < i; j++) {
+//                    dist[i][j] = dimensionScanner.nextDouble();
+//                    dist[j][i] = dist[i][j];
+//                }
+//            }
+//        }
 
         return dist;
     }
-
 }
