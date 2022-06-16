068b3d68ef0c9c1ef892067cf054e9ddda577d8e
Lars Rickert <dev@lars-rickert.de>
2022-06-16 13:05:20 +0200

    refactor: simplified parameter optimizer

--- src/algorithms/ACOParameterOptimizer.java
+++ src/algorithms/ACOParameterOptimizer.java
@@ -13,30 +13,28 @@ public class ACOParameterOptimizer {
 
     public void start() throws IOException, InterruptedException {
         long runtimeStart = System.currentTimeMillis();
+        this.logger.info("Start parameter optimizer for ACO");
         ACOParameters bestParameters = null;
         Route bestRoute = null;
 
-        for (double initialPheromoneValue = 0.1; initialPheromoneValue <= 1; initialPheromoneValue += 0.1) {
-            for (double randomFactor = 0.005; randomFactor <= 0.1; randomFactor += 0.005) {
-                for (double evaporation = 0.05; evaporation <= 1; evaporation += 0.05) {
-                    for (int q = 100; q <= 1000; q += 100) {
-                        for (double alpha = 0.1; alpha <= 2; alpha += 0.1) {
-                            for (double beta = 0.1; beta <= 2; beta += 0.1) {
-                                for (double antFactor = 0.1; antFactor <= 1; antFactor += 0.1) {
-                                    for (int maximumIterations = 1; maximumIterations <= 12; maximumIterations++) {
-                                        ACOParameters parameters = new ACOParameters(initialPheromoneValue, alpha, beta, evaporation, q, antFactor, randomFactor, maximumIterations);
-                                        AntColonyOptimization aco = new AntColonyOptimization(parameters, true);
-                                        Route route = aco.start();
+        double initialPheromoneValue = 1.0;
+        int maximumIterations = 50;
+        int q = 500;
+        double randomFactor = 0.01;
 
-                                        double routeCost = route.getTotalCost();
-                                        if (bestRoute == null || bestRoute.getTotalCost() > routeCost) {
-                                            bestParameters = parameters;
-                                            bestRoute = route;
-                                            this.logger.info("New best parameters | costs: " + routeCost + " | " + parameters);
-                                        }
-                                    }
-                                }
-                            }
+        for (double evaporation = 0.1; evaporation <= 1; evaporation += 0.1) {
+            for (double alpha = 1; alpha <= 6; alpha++) {
+                for (double beta = 1; beta <= 6; beta++) {
+                    for (double antFactor = 1; antFactor >= 0.4; antFactor -= 0.1) {
+                        ACOParameters parameters = new ACOParameters(initialPheromoneValue, alpha, beta, evaporation, q, antFactor, randomFactor, maximumIterations);
+                        AntColonyOptimization aco = new AntColonyOptimization(parameters, true);
+                        Route route = aco.start();
+
+                        double routeCost = route.getTotalCost();
+                        if (bestRoute == null || bestRoute.getTotalCost() > routeCost) {
+                            bestParameters = parameters;
+                            bestRoute = route;
+                            this.logger.info("New best parameters | costs: " + routeCost + " | " + parameters);
                         }
                     }
                 }
