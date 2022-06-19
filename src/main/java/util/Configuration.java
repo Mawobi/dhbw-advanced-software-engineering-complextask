0ffea74a8e84045b41b13d5c650ba183646e0ff3
Lars Rickert <dev@lars-rickert.de>
2022-06-19 18:24:25 +0200

    change parameters

--- src/main/java/util/Configuration.java
+++ src/main/java/util/Configuration.java
@@ -19,11 +19,11 @@ public enum Configuration {
 
     // ACO:
     public final double initialPheromoneValue = 1;
-    public final double alpha = 3;              // pheromone importance
-    public final double beta = 4;               // distance priority
-    public final double evaporation = 1.0;
+    public final double alpha = 1;              // pheromone importance
+    public final double beta = 5;               // distance priority
+    public final double evaporation = 0.5;
     public final double q = 500;                // pheromone left on trail per ant
-    public final double antFactor = 0.9;        // no ants per node
+    public final double antFactor = 0.8;        // no ants per node
     public final double randomFactor = 0.01;    // introducing randomness
-    public final int maximumIterations = 50;
+    public final int maximumIterations = 500;
 }
